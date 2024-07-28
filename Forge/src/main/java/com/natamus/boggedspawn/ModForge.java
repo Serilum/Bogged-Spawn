634ab359b0db12537acfb27f1c3d274f6dc5573f
Rick South <rick@natamus.com>
2024-07-28 07:41:45 +0200

    (v1.1) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/boggedspawn/ModForge.java
+++ Forge/src/main/java/com/natamus/boggedspawn/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.boggedspawn;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.boggedspawn.forge.config.IntegrateForgeConfig;
 import com.natamus.boggedspawn.util.Reference;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,6 +14,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
--- NeoForge/src/main/java/com/natamus/boggedspawn/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/boggedspawn/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.boggedspawn;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.boggedspawn.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.boggedspawn.util.Reference;
 import net.neoforged.bus.api.IEventBus;
@@ -12,6 +13,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
